605cab1f79560a9568e467cb07c0ded1e3a39137
Sussie Anie <sussieanie@hotmail.com>
2022-12-08 15:29:56 +0000

    models

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes',
+    'quotes',
+    'users',
+    'comments'
 ]
 
 MIDDLEWARE = [
