6b9f49c1c0d5f0b976c2485473c61d0c456586b2
Sussie Anie <sussieanie@Sussies-MacBook-Air.local>
2022-12-13 21:46:56 +0000

    added seeds!

--- client/src/components/pages/Gallery.js
+++ client/src/components/pages/Gallery.js
@@ -1,19 +1,61 @@
 // React Components
+import { useState, useEffect, React } from 'react'
+import axios from 'axios'
+
+//Imports from Bootstrap
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
 import { Link } from 'react-router-dom'
-import React, { useState } from 'react';
 
 
 const Gallery = () => {
 
+  //! state
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        const { data } = await axios.get('/quotes/')
+        setNotes(data)
+        console.log(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getNotes()
+  }, [])
+
   return (
     <main className='main'>
-    <div className='home-hero'>
-      <h1 className='display-3'>notes & quotes</h1>
-    </div>
-    <div className='home-article'>
-      <div className='sample-notes'>
+      <div className='home-hero'>
+        <h1 className='display-3'>notes & quotes</h1>
       </div>
-    </div>
+      <div className='notes-container'>
+        <Container className='mt-4'>
+          <Row>
+            {notes.length > 0 &&
+              notes.map(note => {
+                const { id, Content, } = note
+                console.log(id)
+                console.log(Content)
+                return (
+                  <Col key={id} md='6' lg='4' className='note-card mb-4'>
+                      <Card>
+                        <div className='card-image'></div>
+                        <Card.Body>
+                          <Card.Title className='mb-0'>{Content}</Card.Title>
+                        </Card.Body>
+                      </Card>
+                  </Col>
+                )
+              })
+            }
+          </Row>
+        </Container>
+      </div>
   </main>
   )
 }
--- client/src/components/pages/Upload.js
+++ client/src/components/pages/Upload.js
@@ -1,9 +1,40 @@
 // React Components
 import { Link } from 'react-router-dom'
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import axios from 'axios';
+
+
+
 
 
 const Upload = () => {
+
+  const [uploadField, setUploadField] = useState({
+    Content: '', //submitting to the content key
+  })
+
+  const [error, setError] = useState('')
+
+  const handleChange = (e) => {
+    const updatedField = {
+      ...uploadField,
+      [e.target.name]: e.target.value,
+    }
+    setUploadField(updatedField)
+  }
+
+  //submit form
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    console.log(uploadField)
+    try {
+      await axios.post('/notes', uploadField)
+      console.log('post created')
+    } catch (err) {
+      console.log(err.response.data.message)
+      setError(err.response.data.message)
+    }
+  }
 
   return (
     <main className='main'>
